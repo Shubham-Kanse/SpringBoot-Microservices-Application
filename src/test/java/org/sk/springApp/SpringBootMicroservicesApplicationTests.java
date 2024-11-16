0febe397d31143611e9b94146568e19a8cb842db
Shubham-Kanse <shubh.kanse1@gmail.com>
2024-11-16 14:59:52 +0000

    Initial changes

---
+++ src/test/java/org/sk/springApp/SpringBootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sk.springApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
